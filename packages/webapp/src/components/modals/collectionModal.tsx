b40c1c79a5c663feed8140cab1600fd0356367c0
geniusgarlic <geniusgarlicweb3@gmail.com>
2023-03-25 13:01:06 +0100

    Collection page first draft

--- packages/webapp/src/components/modals/collectionModal.tsx
+++ packages/webapp/src/components/modals/collectionModal.tsx
@@ -1,28 +1,23 @@
+import { useRouter } from 'next/router';
+
 export const CollectionGameModal = () => {
 
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/collection');
+  };
 
   return (
     <>
       <label
         htmlFor="collection"
         className="hover:border-3 btn-lg btn border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800"
+        onClick={() => handleClick()}
       >
         Collection →
       </label>
 
-
-      <input type="checkbox" id="collection" className="modal-toggle" />
-      <label htmlFor="collection" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-xl font-bold normal-case">Collection</h3>
-          <p className="py-4">
-            This will bring you to your collection of cards.
-          </p>
-          <button className="btn" onClick={() => console.log(1)}>
-            Collection
-          </button>
-        </label>
-      </label>
+      
 
     </>
   );
--- packages/webapp/src/pages/collection.tsx
+++ packages/webapp/src/pages/collection.tsx
@@ -1,8 +1,104 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { Navbar } from "../components/navbar";
+import React, { useState } from 'react';
+
+//quick fix for testing
+const cards = [
+  {
+    name: "Magician1",
+    description: "A powerful magician",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/0",
+  },
+  {
+    name: "Dwarf1",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/1",
+  },
+  {
+    name: "Dwarf2",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/2",
+  },
+  {
+    name: "Dwarf3",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/3",
+  },
+  {
+    name: "Dwarf4",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/4",
+  },
+  {
+    name: "Warrior",
+    description: "A fierce warrior, having fought many battles.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/5",
+  },
+  {
+    name: "Dwarf5",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/6",
+  },
+  {
+    name: "Dwarf6",
+    description: "A sturdy dwarven druid.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/7",
+  },
+  {
+    name: "Goblin1",
+    description: "A small, green, mean, and ugly creature.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/8",
+  },
+  {
+    name: "Goblin2",
+    description: "A small, green, mean, and ugly creature.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/9",
+  },
+  {
+    name: "Magician2",
+    description: "A powerful magician.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/0",
+  },
+  {
+    name: "Magician3",
+    description: "A powerful magician.",
+    attack: 1,
+    defense: 1,
+    image: "/card_art/0",
+  },
+];  
+
 
 const Play: NextPage = () => {
+  const [selectedCard, setSelectedCard] = useState(cards[0]);
+
+  const handleClick = (card) => {
+    setSelectedCard(card);
+  };
+
   return (
     <>
       <Head>
@@ -14,8 +110,28 @@ const Play: NextPage = () => {
       <main className="flex min-h-screen flex-col">
         <Navbar />
         <div className="grid-col-2 mx-6 mb-6 grid grow grid-cols-12 gap-4">
-          <div className="col-span-3 grow rounded-xl border">...</div>
-          <div className="col-span-9 h-full rounded-xl border">...</div>
+          <div className="col-span-3 grow rounded-xl border">
+            search box / filters by others (attack, defense, etc.)
+            card displayed (what should it be at the start?)
+            <div key={selectedCard.name} className="m-4 bg-slate-900/50 rounded-lg p-4 border-4 border-slate-900">
+              <img src={selectedCard.image} alt={selectedCard.name} className="w-64 h-64" />
+              <div className="text-center">{selectedCard.name}</div>
+              <div className="text-center">{selectedCard.description}</div>
+            </div>
+
+          </div>
+          <div className="col-span-9 h-full rounded-xl border overflow-y-auto h-screen">
+            <div className="grid grid-cols-4 gap-4">
+              {cards.map((card) => (
+                <div key={card.name} className="m-4 bg-slate-900/50 hover:bg-slate-800 rounded-lg p-4 border-4 border-slate-900" onClick={() => handleClick(card)}>
+                  <img src={card.image} alt={card.name} className="w-64 h-64" />
+                  <div className="text-center">{card.name}</div>
+                  <div className="text-center">{card.description}</div>
+                </div>
+              ))}
+              <div className="h-20"></div> {/*to add padding at the bottom*/}
+            </div>
+          </div>
         </div>
       </main>
     </>
