84ece6afea72cb12fc6248d25591a8d811a629d6
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-04-15 14:37:32 +0900

    JoinGameModal debounc + misc improvements

--- packages/webapp/src/components/modals/joinGameModal.tsx
+++ packages/webapp/src/components/modals/joinGameModal.tsx
@@ -7,19 +7,21 @@ import { useWaitForTransaction } from "wagmi"
 import { BigNumber } from "ethers"
 import { constants } from "ethers/lib"
 import * as store from "src/store"
-import { useState } from "react"
+import {useMemo, useState} from "react"
 import { useRouter } from "next/router"
 import { deployment } from "deployment"
 import { useAtom } from "jotai"
+import debounce from 'lodash/debounce'
 
 export const JoinGameModal = () => {
-  const [inputGameID, setInputGameID] = useState(null);
+  const [ inputGameID, setInputGameID ] = useState(null)
   const [ _, setGameID ] = useAtom(store.gameID)
-  const router = useRouter();
+  const router = useRouter()
+  const gameContract = useGame({ address: deployment.Game })
 
-  const gameContract = useGame({
-    address: deployment.Game,
-  });
+  // NOTE(norswap): Right now, the hook can cause error when you type a number that is not a valid
+  //   game ID. This is fine. Alternatively, we could validate the input game ID and enable the hook
+  //   only when the ID is valid.
 
   const { config } = usePrepareGameJoinGame({
     address: deployment.Game,
@@ -34,27 +36,35 @@ export const JoinGameModal = () => {
         ]
       : undefined,
     enabled: inputGameID != undefined,
-  });
+  })
 
-  const { data, write } = useGameJoinGame(config);
+  const { data, write } = useGameJoinGame(config)
 
   useWaitForTransaction({
     hash: data?.hash,
     onSuccess(data) {
-      const event = gameContract.interface.parseLog(data.logs[0]);
-      setGameID(event.args.gameID);
-      router.push("/play");
-    },
-  });
+      const event = gameContract.interface.parseLog(data.logs[0])
+      setGameID(event.args.gameID)
+      router.push("/play")
+    }
+  })
 
-  //check if string is a postive integer
-  const isPositiveInteger = (str: string) => {
-    const n = Math.floor(Number(str));
-    return n !== Infinity && String(n) === str && n >= 0;
-  };
+  // Check if string is a postive integer.
+  function isPositiveInteger(str: string) {
+    const n = Math.floor(Number(str))
+    return n !== Infinity && String(n) === str && n >= 0
+  }
+
+  function handleInputChangeBouncy(e) {
+    e.stopPropagation()
+    if (isPositiveInteger(e.target.value))
+      setInputGameID(e.target.value)
+  }
+  const handleInputChange = useMemo(() => debounce(handleInputChangeBouncy, 300), [])
 
   return (
     <>
+      {/* Button Code */}
       <label
         htmlFor="join"
         className="hover:border-3 btn-lg btn border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800"
@@ -62,23 +72,19 @@ export const JoinGameModal = () => {
         Join →
       </label>
 
+      {/* Modal Code */}
       <input type="checkbox" id="join" className="modal-toggle" />
       <label htmlFor="join" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
+        <label className="modal-box relative">
           <h3 className="text-lg font-bold">Joining Game...</h3>
           <p className="py-4">Enter the game ID you want to join.</p>
           <input
             type="number"
             placeholder="Game ID"
             min={0}
-            onChange={(e) => {
-              setInputGameID(
-                isPositiveInteger(e.target.value) ? e.target.value : null
-              );
-            }}
-            className="input-bordered input-warning input mr-2 w-full max-w-xs"
+            onChange={handleInputChange}
+            className="input input-bordered input-primary mr-2 w-full max-w-xs text-white placeholder-gray-500"
           />
-
           <button
             className="btn"
             disabled={!inputGameID || !write}
@@ -91,5 +97,5 @@ export const JoinGameModal = () => {
         </label>
       </label>
     </>
-  );
-};
+  )
+}
