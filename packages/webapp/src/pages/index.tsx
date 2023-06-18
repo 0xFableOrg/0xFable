577ccb531abd2e251727d3ccb61b44203cc0fabd
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-06-19 01:59:19 +0200

    make these button spazz a little more

--- packages/webapp/src/pages/index.tsx
+++ packages/webapp/src/pages/index.tsx
@@ -48,7 +48,7 @@ const Home = () => {
             <CreateGameModal />
             <JoinGameModal />
             <MintDeckModal />
-            <Link className="hover:border-3 btn-lg btn border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800" href={"/collection"}>
+            <Link className="hover:border-3 btn-lg btn btn-neutral border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800" href={"/collection"}>
               Collection →
             </Link>
           </div>
