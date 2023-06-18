67bdb66d1531dc8f5bcdf87dc4def54f37002775
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-06-18 22:33:49 +0200

    unused setLast array extension

--- packages/webapp/src/utils/extensions.ts
+++ packages/webapp/src/utils/extensions.ts
@@ -11,11 +11,16 @@ export {}
 declare global {
   interface Array<T> {
     last(): T;
+    setLast(item: T): void;
   }
 }
 
 Array.prototype.last = function() {
   return this[this.length - 1]
+}
+
+Array.prototype.setLast = function(item) {
+  this[this.length - 1] = item
 }
 
 // =================================================================================================
