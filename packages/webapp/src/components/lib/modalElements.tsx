d544f111c2f4e4fbbef9c3e2d80ddeb5011c49c6
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-06-19 01:59:19 +0200

    make these button spazz a little more

--- packages/webapp/src/components/lib/modalElements.tsx
+++ packages/webapp/src/components/lib/modalElements.tsx
@@ -19,7 +19,7 @@ export const SpinnerWithMargin = () => {
 export const ModalMenuButton = ({ display, label }) => {
   return <button
     onClick={display}
-    className="hover:border-3 btn-lg btn border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800">
+    className="hover:border-3 btn-lg btn btn-neutral border-2 border-green-900 text-2xl normal-case hover:scale-105 hover:border-green-800">
     {label}
   </button>
 }
