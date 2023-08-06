756679d23550949d160fc58c575388f793ec0dc4
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-08-06 15:45:26 +0200

    introduce a new `actions` directory for user-initiated action logic
    Implement the first action: `joinGame`, which joins the game which by sending a
    `joinGame` transaciton, generating the drawn cards, and then sending a
    `drawInitialHand` transaction (will later also include proof generation).
    
    The draw logic was split up into an isolated game logic in the game package and
    some glue code in the `joinGame` action.

---
+++ packages/webapp/src/actions/errors.ts
@@ -0,0 +1,207 @@
+/**
+ * Errors that can be thrown as a result of a user action, and associated utilities.
+ * These should be caught at the latest at the top-level of the action.
+ *
+ * The utilities include default error handling not only for errors defined in this module, but
+ * also elsewhere, including {@link ContractWriteError} and {@link StaleError}.
+ *
+ * @module actions/errors
+ */
+
+import { ContractFunctionRevertedError, UserRejectedRequestError } from "viem"
+
+import { ContractWriteError } from "src/actions/libContractWrite"
+import { GIT_ISSUES } from "src/constants"
+import { setError } from "src/store/actions"
+
+import { StaleError } from "src/store/read"
+
+// =================================================================================================
+// ERRORS
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Thrown when an operation (usually a network request) times out.
+ */
+export class FableTimeoutError extends Error {
+  constructor(msg: string) {
+    super(msg)
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Thrown when an inconsistent game state is detected. This is an error that should in principle
+ * never be thrown and denotes an implementatin bug, as we strive to include checks to avoid such
+ * inconsistencies (in particular via {@link checkFresh}, and throwing {@link StaleError} whenever
+ * the state shifted underneath an async operation).
+ */
+export class InconsistentGameStateError extends Error {
+  constructor(msg: string) {
+    super(msg)
+  }
+}
+
+// =================================================================================================
+// ERROR HANDLING
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Performs default error handling for user actions: if the error is of a known type, it is handled,
+ * and false returned. If the error is unknown, it is rethrown.
+ *
+ * Handling is done by setting the global error state, which is then displayed by the UI to report
+ * the issue to the user, and give him options for handling it (though usually not much can be done
+ * besides retrying or refresh the page).
+ *
+ * For {@link StaleError}, no error is displayed, as the UI should have already updated from the
+ * store changes that caused the error to be thrown.
+ */
+export function defaultErrorHandling(actionName: string, err: unknown): false {
+
+  if (err instanceof StaleError)
+    return false
+
+  if (err instanceof FableTimeoutError) {
+    setError({
+      title: "Request timed out.",
+      message: (err as Error).message + " Please try again.",
+      buttons: [DISMISS_BUTTON]
+    })
+    return false
+  }
+
+  if (err instanceof ContractWriteError)
+    return defaultContractWriteErrorHandling(err)
+
+  if (err instanceof InconsistentGameStateError) {
+    reportInconsistentGameState(err)
+    return false
+  }
+
+  throw err
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Performs default error handling for {@link ContractWriteError} thrown by {@link
+ * contractWriteThrowing}.
+ *
+ * Just like {@link defaultErrorHandling}, this sets the global error state to report issues to the
+ * user.
+ *
+ * Unlike {@link defaultErrorHandling}, unknown exceptions wrapped by {@link ContractWriteError} are
+ * never rethrown.
+ */
+export function defaultContractWriteErrorHandling(err: ContractWriteError): false {
+
+  if (err.result.revert) {
+    // Ethereum nodes do not store the revert reason at all, so there is no way to be more
+    // precise than this. These errors can only occur when the call succeeded at simulation time
+    // but failed at executing on-chain, which should make them rare. This also means they are
+    // caused by a race condition in the contract, where another action changed the state such
+    // that a previously successful call now fails. We should try to avoid these as a matter of
+    // design.
+
+    // It's also hard to know if the error was due to a logic revert or to running
+    // out of gas. This is a viem issue, see here for the explanation:
+    // https://twitter.com/norswap/status/1687491839320842240
+    // In both cases, the problem is triggered by an underlying state change.
+
+    // Ideally, we override this and handle it in a custom manner only for actions where these
+    // races are possible.
+
+    setError({
+      title: "Contract execution error",
+      message: `Transaction reverted (${err.args.functionName}) at execution time `
+        + "but not at simulation time. Please try again.",
+      buttons: [DISMISS_BUTTON]
+    })
+    return false
+  }
+
+  const error = err.result.error
+
+  if (error instanceof UserRejectedRequestError) {
+    // The user rejected the execution, he's probably well aware of the fact.
+    return false
+  }
+
+  if (error instanceof ContractFunctionRevertedError) {
+    if (error.data) {
+      // The revert was parsed, because the function signature was found in the ABI of the
+      // calling contract (meaning it was declared *inside* the contract).
+      setError({
+        title: "Contract execution error",
+        message: `Transaction reverted (${err.args.functionName}) with `
+          +`${error.data.errorName}(${error.data.args})`,
+        buttons: [DISMISS_BUTTON]
+      })
+    } else {
+      // The revert wasn't parsed, probably because the error isn't declared *inside* the
+      // contract, but comes from another contract and wasn't redeclared.
+
+      const signatureMsg = error.signature
+          ? `with signature ${error.signature}`
+          : "with no signature"
+
+      setError({
+        title: "Contract execution error",
+        message: `Transaction reverted (${err.args.functionName}) ${signatureMsg}.`
+          + `Please report to ${GIT_ISSUES}`,
+        buttons: [DISMISS_BUTTON]
+      })
+    }
+    return false
+  }
+
+  setError({
+    title: "Contract execution error",
+    message: error.toString(),
+    buttons: [DISMISS_BUTTON]
+  })
+
+  return false
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Sets the global error state to report an inconsistent game state to the user.
+ *
+ * As state in {@link InconsistentGameStateError}, this should never occur in practice, and
+ * implies an implementation bug or weakness.
+ */
+export function reportInconsistentGameState(err: InconsistentGameStateError|string) {
+  const message = typeof err === "string" ? err : err.message
+  setError({
+    title: "Error: Inconsistent game state.",
+    message: `${message}\n\nPlease reload the page.`,
+    buttons: [RELOAD_BUTTON, DISMISS_BUTTON]
+  })
+}
+
+// =================================================================================================
+// ERROR UTILITIES
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * A button meant to be used as a component in {@link ErrorConfig.buttons} when passed to {@link
+ * setError}. It dismisses the error when clicked.
+ */
+export const DISMISS_BUTTON = { text: "Dismiss", onClick: () => setError(null) }
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * A button meant to be used as a component in {@link ErrorConfig.buttons} when passed to {@link
+  * setError}. It reloads the page when clicked.
+ */
+export const RELOAD_BUTTON = { text: "Refresh", onClick: () => window.location.reload() }
+
+// =================================================================================================
---
+++ packages/webapp/src/actions/joinGame.ts
@@ -0,0 +1,280 @@
+/**
+ * An user action responsible for making a player join the game, by sending the `joinGame` and
+ * `drawInitialHand` transactions.
+ *
+ * @module action/joinGame
+ */
+
+import { decodeEventLog } from "viem"
+
+import {
+  defaultErrorHandling,
+  DISMISS_BUTTON,
+  FableTimeoutError,
+  InconsistentGameStateError
+} from "src/actions/errors"
+import { contractWriteThrowing } from "src/actions/libContractWrite"
+import { Address, Hash } from "src/chain"
+import { deployment } from "src/deployment"
+import { drawInitialHand } from "src/game/drawInitialHand"
+import { isGameReadyToStart } from "src/game/status"
+import { gameABI } from "src/generated"
+import {
+  getOrInitPrivateInfo, setError,
+  setGameID, setPrivateInfo,
+  waitForUpdate
+} from "src/store/actions"
+import { fetchDeck } from "src/store/network"
+import {
+  checkFresh,
+  freshWrap,
+  getGameData,
+  getGameID,
+  getGameStatus, getPlayerData,
+  getPrivateInfo
+} from "src/store/read"
+import { FetchedGameData, GameStatus, PrivateInfo } from "src/types"
+
+// =================================================================================================
+
+/**
+ * Temporary stand-in value. Don't use 0x0 as that can be interpreted as lack of initialization in
+ * the contracts.
+ */
+const HashOne = "0x0000000000000000000000000000000000000000000000000000000000000001"
+
+// =================================================================================================
+
+export type JoinGameArgs = {
+  gameID: bigint
+  playerAddress: Address
+  setLoading: (label: string|null) => void
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Makes the player join the game, by sending the `joinGame` and `drawInitialHand` transactions.
+ *
+ * Handles both the game creator joining, as well as a player that isn't currently associated
+ * to any game.
+ */
+export async function joinGame(args: JoinGameArgs): Promise<boolean> {
+  try {
+    return await joinGameImpl(args)
+  } catch (err) {
+    args.setLoading(null)
+
+    if (err instanceof FetchDeckError) {
+      setError({
+        title: "Could not fetch deck",
+        message: err.cause.toString(),
+        buttons: [DISMISS_BUTTON]
+      })
+      return false
+    }
+
+    return defaultErrorHandling("joinGame", err)
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+async function joinGameImpl(args: JoinGameArgs): Promise<boolean> {
+
+  const gameID = getGameID()
+  const gameStatus = getGameStatus()
+
+  if (gameStatus >= GameStatus.HAND_DRAWN) // works with UNKNOWN
+    // We already drew, no need to display an error, this is a stale call,
+    // and most likely we're in an aberrant state anwyay.
+    return false
+
+  let privateInfo: PrivateInfo|null = getOrInitPrivateInfo(args.gameID, args.playerAddress)
+
+  // TODO
+  //    Should initiate proof generation now, should that it proceeds in the background while
+  //    sending the joining transactions.
+
+  if (gameStatus < GameStatus.JOINED) {
+    const promise = doJoinGameTransaction(args, privateInfo.saltHash)
+    if (gameID === null)
+      await promise // gameID starts null and the call will set it
+    else
+      checkFresh(await freshWrap(promise))
+  }
+
+  const gameData = getGameData()
+  if (gameData === null) // should be impossible due to checkFresh
+    throw new InconsistentGameStateError("Missing game data.")
+
+  args.setLoading("Drawing cards...")
+
+  privateInfo = getPrivateInfo(args.gameID, args.playerAddress)
+  if (privateInfo === null) // should be impossible due to checkFresh
+    throw new InconsistentGameStateError("Missing private info.")
+
+  privateInfo = checkFresh(await freshWrap(
+    drawCards(args.gameID, args.playerAddress, gameData, privateInfo, 0 /* deckID */)))
+
+  // Skip checkFresh: no more store access after this.
+  await doDrawInitialHandTransaction(args, privateInfo)
+  return true
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Sends the `joinGame` transaction, then waits for the game data to be updated in response.
+ * @throws {StaleError} if the store shifts underneath the transaction.
+ */
+async function doJoinGameTransaction(args: JoinGameArgs, saltHash: Hash) {
+
+  // function joinGame(uint256 gameID, uint8 deckID, bytes32 saltHash, bytes calldata data)
+  const result = checkFresh(await freshWrap(
+    contractWriteThrowing({
+      contract: deployment.Game,
+      abi: gameABI,
+      functionName: "joinGame",
+      args: [
+        args.gameID,
+        0, // deckID
+        saltHash,
+        HashOne, // data for join-check callback
+      ],
+      setLoading: args.setLoading
+    })))
+
+  if (getGameID() === null) {
+    // gameID is null and needs to be set
+    const logs = result.receipt.logs
+    const event = decodeEventLog({
+      abi: gameABI,
+      data: logs[0].data,
+      topics: logs[0]["topics"]
+    })
+    setGameID(event.args["gameID"])
+  }
+
+  // We need to get the player data in order to draw cards.
+  // NOTE: No UI way to navigate away, so no risk to write to a stale component state.
+  args.setLoading("Waiting for update...")
+  const gameData = checkFresh(await freshWrap(waitForUpdate(result.receipt.blockNumber)))
+
+  if (gameData === null)
+    // Null can also be returned if the game data was cleared, but this should be caught by
+    // checkFresh, and an exception thrown.
+    throw new FableTimeoutError("Timed out waiting for up to date game data.")
+
+  // NOTE: Drawing could be done optimistically, though we'll always need a check that the order
+  // is indeed what we believed or we risk having the wrong card indexes if a player joined
+  // after we started the draw simulation.
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Sends the `drawInitialHand` transaction, then sets the loading state to "Loading game..." if
+ * the game is ready to start.
+ */
+async function doDrawInitialHandTransaction(args: JoinGameArgs, privateInfo: PrivateInfo) {
+
+  // function drawInitialHand(uint256 gameID, bytes32 handRoot, bytes32 deckRoot, bytes calldata proof)
+  const result = checkFresh(await freshWrap(
+    contractWriteThrowing({
+      contract: deployment.Game,
+      abi: gameABI,
+      functionName: "drawInitialHand",
+      args: [
+        args.gameID,
+        privateInfo.handRoot,
+        privateInfo.deckRoot,
+        HashOne, // proof
+      ],
+      setLoading: args.setLoading,
+    })))
+
+  const gameData = getGameData()
+  if (gameData === null) // should be impossible due to checkFresh
+    throw new InconsistentGameStateError("Missing game data.")
+
+  // Assuming two players, if we're the last to draw, we just need to wait for (1) the data
+  // refresh and (2) loading of the play page. Not displaying a loading modal would just show
+  // the old screen, which is janky (feels like our join didnt work).
+  // The alternative is an optimistic update of the game status & data.
+  if (isGameReadyToStart(gameData, result.receipt.blockNumber))
+    // NOTE: possible stale component site
+    args.setLoading("Loading game...")
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Fetches the player's deck, then computes the initial hand and deck state for the player, as well
+ * as their respective commitments, and store them in the private info store.
+ *
+ * @returns the updated private info
+ * @throws {FetchDeckError} if the deck could not be fetched
+ */
+export async function drawCards
+    (gameID: bigint, player: Address, gameData: FetchedGameData,
+     privateInfo: PrivateInfo, deckID: number)
+    : Promise<PrivateInfo>{
+
+  const deck = [... checkFresh(await freshWrap(wrappedFetchDeck(player, deckID)))]
+
+  // NOTE: No need to update the game data or the private info here.
+  // They couldn't possibly have change.
+
+  const playerData = getPlayerData(gameData, player)
+  if (playerData === null) throw new InconsistentGameStateError("Missing player data.")
+
+  const handDeckInfo = drawInitialHand(
+      deck, playerData.deckStart, privateInfo.salt, gameData.publicRandomness)
+
+  console.log(`drew initial hand: ${handDeckInfo.hand}`)
+
+  // TODO validate proofs — the setup has been tested, no we just need the actual production proofs
+  // TODO this will need to run in a worker thread, not to hogg the main thread
+  // checkFresh(await freshWrap(
+  //   testProving("MerkleHand", { root: handRoot, leaves: extendedHand })))
+  // NOTE: MerkleHand is a circuit defined as:
+  //       component main { public [root, leaves] } = CheckMerkleRoot(6);
+
+  const newPrivateInfo = {
+    salt: privateInfo.salt,
+    saltHash: privateInfo.saltHash,
+    ...handDeckInfo,
+  }
+
+  setPrivateInfo(gameID, player, newPrivateInfo)
+  return newPrivateInfo
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Wraps an error thrown by {@link fetchDeck}.
+ */
+class FetchDeckError extends Error {
+  cause: any
+  constructor(cause: any) {
+    super(`Failed to fetch deck: ${cause}`)
+    this.cause = cause
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Calls {@link fetchDeck}, wrapping any thrown error in a {@link FetchDeckError}.
+ */
+async function wrappedFetchDeck(player: Address, deckID: number): Promise<readonly bigint[]> {
+  try {
+    return await fetchDeck(player, deckID)
+  } catch (err) {
+    throw new FetchDeckError(err)
+  }
+}
+
+// =================================================================================================
