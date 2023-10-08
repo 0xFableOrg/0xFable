d6e4fb00d0d7e8d2e963d263dd8b3167d62d6ef9
ultraviolet10 <aritrac1998@hotmail.com>
2023-10-08 19:29:01 +0530

    Implement a horizontal card drawer component #25

--- packages/webapp/src/components/hand.tsx
+++ packages/webapp/src/components/hand.tsx
@@ -1,7 +1,5 @@
-// import useScrollBox from "@/hooks/useScrollBox";
-// import Image from "next/image";
-// import { useRouter } from "next/router";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import useScrollBox from "../hooks/useScrollBox";
 import { Card } from "./card";
 
@@ -12,34 +10,64 @@ const Hand = ({
   cards: bigint[];
   className?: string;
 }) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
   const hand: any = [];
   const scrollWrapperRef = useRef<any>();
-  const { isDragging } = useScrollBox(scrollWrapperRef);
+  const { isDragging, showLeftArrow, scrollLeft, showRightArrow, scrollRight } =
+    useScrollBox(scrollWrapperRef);
 
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards?.length; i++) {
     hand.push(
-      <div key={i} className="-mx-20">
-        {/* <div className="absolute inset-0 z-50 flex cursor-pointer items-center justify-center rounded-md bg-blue-900/20  text-slate-200 opacity-0 transition-all group-hover:opacity-100">
-          <p className="bg-slate-900 py-2 pl-0 pr-2">view on opensea</p>
-        </div> */}
+      <div key={i}>
         <Card
           id={i}
           className="transitional-all duration-200 hover:scale-[100%] hover:border-yellow-500"
+          hover={isFocused}
         />
       </div>
     );
   }
+
   return (
     <div
-      className={`${className} noScrollbar absolute bottom-0 w-full overflow-scroll`}
-      ref={scrollWrapperRef}
+      className={`${className} flex flex-row items-center justify-evenly absolute bottom-0 z-[100] w-[95%]`}
+      onMouseEnter={() => {
+        setIsFocused(true);
+      }}
+      onMouseLeave={() => {
+        setIsFocused(false);
+      }}
     >
-      <div
-        className="relative flex w-max"
-        style={{ pointerEvents: isDragging ? "none" : undefined }}
-      >
-        {hand}
+      {showLeftArrow && isFocused && (
+        <div
+          className="absolute top-[55%] transform -translate-y-1/2 left-0 z-10 p-2 cursor-pointer"
+          onClick={scrollLeft}
+        >
+          <AiOutlineLeft className="text-white text-[50px]" />
+        </div>
+      )}
+      <div className={`relative w-[90%] overflow-x-hidden overflow-y-visible`}>
+        <div
+          className="overflow-x-scroll"
+          ref={scrollWrapperRef}
+          style={{ pointerEvents: isDragging ? "none" : undefined }}
+        >
+          <div className="relative flex w-max">
+            <div className="flex flex-row items-end justify-center space-x-4 px-2">
+              {hand}
+            </div>
+          </div>
+        </div>
       </div>
+      {showRightArrow && isFocused && (
+        <div
+          className="absolute top-[55%] transform -translate-y-1/2 right-0 z-10 p-2 cursor-pointer"
+          onClick={scrollRight}
+        >
+          <AiOutlineRight className="text-white text-[50px]" />
+        </div>
+      )}
     </div>
   );
 };
