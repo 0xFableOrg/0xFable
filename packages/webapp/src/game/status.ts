30730274cef09513101b1713276507687ccfb630
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-12 18:16:00 +0100

    better isGameReadyToStart documentation

--- packages/webapp/src/game/status.ts
+++ packages/webapp/src/game/status.ts
@@ -35,12 +35,18 @@ export function getGameStatus(gdata: FetchedGameData|null, player: Address|null)
 // -------------------------------------------------------------------------------------------------
 
 /**
- * Whether all player have joined and drawn their initial hands.
+ * Whether all player have joined and drawn their initial hands, given available block info and a
+ * block number.
+ *
+ * Note that the block number might not be in sync with the game data. This is only so that we can
+ * handle the case where the block number is the one at which we included our `drawInitialHand`
+ * proof, and the game data hasn't updated accordingly yet.
  */
 export function isGameReadyToStart(gameData: FetchedGameData, blockNumber: bigint): boolean {
   return gameData.playersLeftToJoin === 0 &&
     (gameData.lastBlockNum >= blockNumber
-      // depending on whether the data has already been updated with the results of the draw call
+      // Depending on whether the game data has already been updated with the results of the
+      // drawInitialHand call.
       ? gameData.livePlayers.length === gameData.players.length
       : gameData.livePlayers.length === gameData.players.length - 1)
 }
