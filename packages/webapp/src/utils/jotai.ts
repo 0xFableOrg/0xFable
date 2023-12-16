2804d879e57f538a9e2436456f94aa841bb495fb
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-16 18:59:49 +0100

    cache atoms to avoid spurious re-renders

---
+++ packages/webapp/src/utils/jotai.ts
@@ -0,0 +1,31 @@
+/**
+ * Utilities to deal with the Jotai state management library.
+ *
+ * @module utils/jotai
+ */
+
+import { atom, Atom } from "jotai"
+import { isEqual } from "lodash"
+
+// =================================================================================================
+
+/** Re-export simplified definition of jotai Getter type. */
+export type Getter = <Value>(atom: Atom<Value>) => Value
+
+/** Re-export simplified definition of jotai Read type. */
+export type Read<Value> = (get: Getter) => Value
+
+/**
+ * Similar to a read-only derived atom, but the result will be cached via deep-comparison,
+ * to avoid spurious re-renders.
+ */
+export function cachedAtom<Value>(read: Read<Value>): Atom<Value> {
+  let cache: Value | null = null
+  return atom((get) => {
+    const value = read(get as any)
+    if (!isEqual(value, cache)) cache = value
+    return cache!
+  })
+}
+
+// =================================================================================================
