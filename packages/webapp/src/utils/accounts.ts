30989a14af0cd9af43d12b3c205fdec3b1be798c
eerkaijun <kaijuneer@gmail.com>
2024-01-13 22:51:15 +0000

    paymaster signature validation works

--- packages/webapp/src/utils/accounts.ts
+++ packages/webapp/src/utils/accounts.ts
@@ -4,22 +4,31 @@
  * @module utils/accounts
  */
 
-import { http, parseEther} from 'viem'
+import axios from 'axios'
+import { Address, http, parseEther, defineChain, toHex, concat } from 'viem'
 import { localhost } from 'viem/chains'
 import { getPublicClient } from "wagmi/actions"
 import { generatePrivateKey } from 'viem/accounts'
-import { createBundlerClient, createSmartAccountClient } from 'permissionless'
+import { createSmartAccountClient, UserOperation } from 'permissionless'
 import { privateKeyToSimpleSmartAccount } from 'permissionless/accounts'
 
 const publicClient = getPublicClient()
 
-// TODO: we need to initialize paymaster contract?
-
 export async function createAccount() {
+
+    const rollop = defineChain({
+        ...localhost,
+        id: 1201101712,
+        name: 'Rollop',
+        nativeCurrency: {
+          decimals: 18,
+          name: 'Ether',
+          symbol: 'ETH',
+        },
+    });
 
     // generate an in browser private key
     const privateKey = generatePrivateKey()
-
     // TODO: store this private key in atom
 
     // create a simple account where the private key is the signer
@@ -28,24 +37,52 @@ export async function createAccount() {
         factoryAddress: "0x9406Cc6185a346906296840746125a0E44976454",
         entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
     });
-    console.log("Simple account: ", simpleAccount)
 
     // create a smart account client (equivalent to a walletClient)
     const smartAccountClient = createSmartAccountClient({
+        chain: rollop,
         account: simpleAccount,
-        chain: localhost,
-        transport: http(
-            "http://localhost:4337",
-        ),
-        // sponsorUserOperation: paymasterClient.sponsorUserOperation, // optional
+        transport: http("http://localhost:4337"),
+        sponsorUserOperation: async (args: { userOperation: UserOperation, entryPoint: Address }) => {
+            // console.log("Breakpoint1")
+            const userOp = {
+                sender: args.userOperation.sender,
+                nonce: toHex(args.userOperation.nonce),
+                initCode: args.userOperation.initCode,
+                callData: args.userOperation.callData,
+                paymasterAndData: concat([
+                    '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+                    '0x' + '00'.repeat(64) as `0x${string}`,
+                    '0x' + '00'.repeat(65) as `0x${string}`
+                ]),
+                signature: args.userOperation.signature,
+                maxFeePerGas: toHex(2_000_000_000),
+                maxPriorityFeePerGas: toHex(1_000_000_000),
+                callGasLimit: toHex(3_000_000),
+                verificationGasLimit: toHex(3_000_000),
+                preVerificationGas: toHex(2_000_000)
+            }
+            const jsonRpcRequest = {
+                jsonrpc: '2.0',
+                method: 'pm_sponsorUserOperation',
+                params: userOp,
+                id: 1
+            };
+            const response = await axios.post("http://localhost:3000", jsonRpcRequest, {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+            });
+            return response.data.result;
+        }
     });
-    console.log("Smart account client: ", smartAccountClient);
 
-    // Generalize sending transaction
-
-    // const txHash = await smartAccountClient.sendTransaction({
-    //     to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-    //     value: parseEther("0.1"),
-    // });
-    // console.log("Transaction hash: ", txHash);
+    // to send a transaction, it will be similar to using walletClient, example below
+    /**
+    const txHash3 = await smartAccountClient.sendTransaction({
+        to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+        value: BigInt(0),
+        data: "0x68656c6c6f"
+    });
+    */
 }
