5b66baf623ae6b9af21560383d089a9a25efbc39
Aritra Chatterjee <aritrac1998@hotmail.com>
2024-01-17 00:33:45 +0530

    update modals with `Dialog` component

--- packages/webapp/src/components/modals/gameEndedModal.tsx
+++ packages/webapp/src/components/modals/gameEndedModal.tsx
@@ -1,21 +1,30 @@
 import { useRouter } from "next/router"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 
-import { ModalTitle } from "src/components/lib/modalElements"
 import { useGameData, useGameID } from "src/store/hooks"
-import { Modal, useModalController } from "src/components/lib/modal"
 import { navigate } from "src/utils/navigate"
+import {
+  Dialog,
+  DialogDescription,
+  DialogTitle,
+  DialogContent,
+} from "../ui/dialog"
+import { Button } from "../ui/button"
 
 /**
  * This modal is displayed in the "/play" page, when the game ends. It can be dismissed if the
  * player wishes to view the final state of the game board, after which he can still go back
  * to the menu through a button on the game board.
  */
-export const GameEndedModal = ({ closeCallback }: { closeCallback: () => void }) => {
-  const ctrl = useModalController({ displayed: true, closeable: false })
+export const GameEndedModal = ({
+  closeCallback,
+}: {
+  closeCallback: () => void
+}) => {
   const router = useRouter()
   const [ , setGameID ] = useGameID()
   const gameData = useGameData()
+  const [open, isOpen] = useState<boolean>(true)
 
   const exitToMenu = useCallback(() => {
     setGameID(null)
@@ -23,20 +32,28 @@ export const GameEndedModal = ({ closeCallback }: { closeCallback: () => void })
   }, [router, setGameID])
 
   const viewGame = useCallback(() => {
-    ctrl.close()
+    isOpen(false)
     closeCallback()
-  }, [closeCallback, ctrl])
+  }, [closeCallback])
 
-  return <Modal ctrl={ctrl}>
-    <ModalTitle>Game Ended</ModalTitle>
-    <p className="py-4 font-mono">Winner: {gameData?.players[gameData.livePlayers[0]]}</p>
-    <div className="flex justify-center gap-4">
-      <button className="btn" onClick={exitToMenu}>
-        Exit to Menu
-      </button>
-      <button className="btn" onClick={viewGame}>
-        View Game
-      </button>
-    </div>
-  </Modal>
+  return (
+    <Dialog open={open}>
+      <DialogContent>
+        <DialogTitle>Game Ended</DialogTitle>
+        <DialogDescription>
+          <p className="py-4 font-mono">
+            Winner: {gameData?.players[gameData.livePlayers[0]]}
+          </p>
+          <div className="flex justify-center gap-4">
+            <Button className="font-fable" variant={"secondary"} onClick={exitToMenu}>
+              Exit to Menu
+            </Button>
+            <Button className="font-fable" variant={"secondary"} onClick={viewGame}>
+              View Game
+            </Button>
+          </div>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  )
 }
