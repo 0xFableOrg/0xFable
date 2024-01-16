4f17825df41ae6ac6f273a45bfa687996c914b30
Aritra Chatterjee <aritrac1998@hotmail.com>
2024-01-17 00:13:16 +0530

    redo navbar

--- packages/webapp/src/components/navbar.tsx
+++ packages/webapp/src/components/navbar.tsx
@@ -1,30 +1,44 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 import { ConnectKitButton } from "connectkit"
+import { Button } from "./ui/button"
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+} from "./ui/navigation-menu"
 
 export const Navbar = () => {
+  const router = useRouter()
+
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <Link
-          href={"/"}
-          className="btn-ghost btn font-serif text-2xl normal-case text-white"
-        >
-          <span className="font-mono font-light text-red-400">0x</span>
-          FABLE
-        </Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/collection" className="font-bold ">
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/">
+            <Button
+              variant="ghost"
+              className="text-2xl normal-case text-white font-serif"
+            >
+              <span className="text-red-400 font-mono font-light">0x</span>
+              FABLE
+            </Button>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+
+      <NavigationMenuList className="space-x-4">
+        <NavigationMenuItem>
+          {router.pathname !== "/collection" && (
+            <Link href="/collection" className="font-bold font-fable">
               COLLECTION
             </Link>
-          </li>
-          <li>
-            <ConnectKitButton />
-          </li>
-        </ul>
-      </div>
-    </div>
+          )}
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <ConnectKitButton />
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   )
 }
