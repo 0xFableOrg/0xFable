0a17342c864aee11fc123aaa7af1b42e7977518b
Aritra Chatterjee <aritrac1998@hotmail.com>
2024-01-18 15:32:46 +0530

    remove extra parent and parentheses

--- packages/webapp/src/pages/index.tsx
+++ packages/webapp/src/pages/index.tsx
@@ -38,57 +38,55 @@ const Home: FablePage = ({ isHydrated }) => {
   const isWrongNetwork = !notConnected && !chainSupported
 
   return (
-    <>
-      <main className="flex flex-col min-h-screen items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="font-serif text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="font-mono font-light text-red-400">0x</span>FABLE
-          </h1>
+    <main className="flex flex-col min-h-screen items-center justify-center">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="font-serif text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <span className="font-mono font-light text-red-400">0x</span>FABLE
+        </h1>
 
-          {notConnected && (
-            <div className="">
-              <Button
-                variant={"secondary"}
-                className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400"
-                onClick={async () => setOpen(true)}
-              >
-                Connect Wallet
-              </Button>
+        {notConnected && (
+          <div className="">
+            <Button
+              variant={"secondary"}
+              className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400"
+              onClick={async () => setOpen(true)}
+            >
+              Connect Wallet
+            </Button>
+          </div>
+        )}
+
+        {isWrongNetwork && <ConnectKitButton />}
+
+        {isRightNetwork && (
+          <>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-8">
+              <CreateGameModal />
+              <JoinGameModal />
+              <MintDeckModal />
+              <Link href={"/collection"}>
+                <Button
+                  variant="outline"
+                  className="rounded-lg p-6 font-fable text-2xl border-green-900 border-2 h-16 hover:scale-105 hover:border-green-800 hover:border-3"
+                >
+                  Collection →
+                </Button>
+              </Link>
+              <Link href={"/editor"}>
+                <Button
+                  variant="outline"
+                  className="rounded-lg p-6 font-fable text-2xl border-green-900 border-2 h-16 hover:scale-105 hover:border-green-800 hover:border-3"
+                >
+                  Editor →
+                </Button>
+              </Link>
             </div>
-          )}
 
-          {isWrongNetwork && <ConnectKitButton />}
-
-          {isRightNetwork && (
-            <>
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-8">
-                <CreateGameModal />
-                <JoinGameModal />
-                <MintDeckModal />
-                <Link href={"/collection"}>
-                  <Button
-                    variant="outline"
-                    className="rounded-lg p-6 font-fable text-2xl border-green-900 border-2 h-16 hover:scale-105 hover:border-green-800 hover:border-3"
-                  >
-                    {` Collection →`}
-                  </Button>
-                </Link>
-                <Link href={"/editor"}>
-                  <Button
-                    variant="outline"
-                    className="rounded-lg p-6 font-fable text-2xl border-green-900 border-2 h-16 hover:scale-105 hover:border-green-800 hover:border-3"
-                  >
-                    {`Editor →`}
-                  </Button>
-                </Link>
-              </div>
-
-              <ConnectKitButton />
-            </>
-          )}
-        </div>
-      </main>
-    </>
+            <ConnectKitButton />
+          </>
+        )}
+      </div>
+    </main>
   )
 }
 
