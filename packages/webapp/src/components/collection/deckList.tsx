9a8eaf92fb10e9a2758adc714587d0fc95d01f5c
eviterin <ethan.viterin@gmail.com>
2024-02-20 18:52:17 +0100

    use our own buttons

--- packages/webapp/src/components/collection/deckList.tsx
+++ packages/webapp/src/components/collection/deckList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from "src/components/link"
 import { Deck } from 'src/store/types'
+import { Button } from "src/components/ui/button"
 
 interface DeckCollectionDisplayProps {
   decks: Deck[]
@@ -11,21 +12,24 @@ const DeckCollectionDisplay: React.FC<DeckCollectionDisplayProps> = ({ decks, on
   return (
       <div className="w-full flex flex-col items-center p-3">
         {/* New Deck Button */}
-        <Link 
-          href={"/collection?newDeck=true"} 
-          className="w-full px-4 py-2 mb-2 border rounded-md text-gray-100 bg-purple-900 hover:bg-gray-500 font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
-          New Deck →
-        </Link>
+        <div>
+        <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400">
+          <Link 
+            href={"/collection?newDeck=true"} 
+            className="w-full px-4 py-2 mb-2 border rounded-md text-gray-100 bg-purple-900 hover:bg-gray-500 font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+            New Deck →
+          </Link>
+        </Button>
+        </div>
 
         {/* Deck Buttons */}
         {decks.map((deck, deckID) => (
-          <button 
+          <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400"
             key={deckID} 
             onClick={() => onDeckSelect(deckID)}
-            className="w-full px-4 py-2 mb-2 border rounded-md text-gray-100 bg-purple-900 hover:bg-gray-500 font-bold text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          >
+            >
             {deck.name}
-          </button>
+          </Button>
         ))}
       </div>
   )
