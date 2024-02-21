41c691ad4c13262097c5504d0d141d3035de7202
Aritra Chatterjee <aritrac1998@hotmail.com>
2024-02-21 19:06:59 +0530

    fix: trigger error modal

--- packages/webapp/src/components/modals/globalErrorModal.tsx
+++ packages/webapp/src/components/modals/globalErrorModal.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
 } from "../ui/dialog"
 import { Button } from "src/components/ui/button"
+import { useEffect, useState } from "react"
 
 /**
  * A modal displayed globally (setup in _app.tsx) whenever the errorConfig state is set to non-null.
@@ -16,9 +17,14 @@ export const GlobalErrorModal = ({ config }: { config: ErrorConfig }) => {
   // UI. This is good practice as it lets the user figure out what happened. Really not a priority
   // at the moment, and the error should be systematically logged to the console instead, for
   // debugging purposes.
+  const [ open, setOpen ] = useState<boolean>(false)
+  useEffect(() => {
+    if(config !== null && !open) setOpen(true)
+    else setOpen(false)
+  }, [config, open])
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTitle>{config.title}</DialogTitle>
       <DialogContent>
         {config.message !== "" && (
