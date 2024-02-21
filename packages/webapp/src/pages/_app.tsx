45e1dc6e21db9ea98f2735a00f240cec7a7ac50d
Patrice Tisserand <p.tisserand@gmail.com>
2024-02-21 20:11:54 +0100

    Fix linter issue on _app.tsx

--- packages/webapp/src/pages/_app.tsx
+++ packages/webapp/src/pages/_app.tsx
@@ -76,7 +76,7 @@ const ComponentWrapper = ({
   const errorConfig = useErrorConfig()
 
     // todo @eviterin: i've understood it so that decks are stored on chain. thus, below part is not going to be needed.
-    const testCards = [];
+    const _testCards = [];
     const [decks, setDecks] = useState<Deck[]>([]);
     //
 
