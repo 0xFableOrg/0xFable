36e35aed807788202a4c1eeb255fafa4866c7827
eviterin <ethan.viterin@gmail.com>
2024-02-22 10:46:24 +0100

    Improved flexing the deck panel

--- packages/webapp/src/components/collection/deckPanel.tsx
+++ packages/webapp/src/components/collection/deckPanel.tsx
@@ -37,51 +37,51 @@ interface DeckConstructionPanelProps {
     }
 
   return (
-    <div className="flex flex-col items-center w-full p-3">
-    <div className="flex flex-wrap justify-between items-center w-full mb-4">
+    <div className="flex flex-col items-center w-full p-3 overflow-y-auto overflow-x-hidden">
       {/* Deck Name Input */}
-      <input
-        type="text"
-        value={deckName}
-        onChange={handleDeckNameChange}
-        style={{ outline: deckNameValid ? "none" : "2px solid red" }}
-        className="flex-grow px-2 py-2 border rounded-md text-black bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent m-1.5 placeholder-gray-700"
-        placeholder="Deck name"
-      />
-    </div>
-
-    {/* Save and Cancel Buttons */}
-    <div className="flex gap-2 justify-center w-full">
-      <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400" onClick={() => handleSave()}>
-        ✓Save
-      </Button>
-      <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400" onClick={() => onCancel()}>
-        ✕Cancel
-      </Button>
-    </div>
-
-    {/* List of Cards in the Deck */}
-    <div className="mt-4 w-full">
-    {selectedCards.length > 0 ? (
-      selectedCards.map((card, index) => (
-        <div 
-          key={index} 
-          className="p-2 cursor-pointer hover:bg-gray-100"
-          onClick={() => onCardSelect(card)}
-        >
-          <div className="flex items-center space-x-3">
-            <Image src={testCards.find(tc => tc.id === index)?.image || '/card_art/1.jpg'} alt="Card art" width={40} height={40} className="object-cover rounded-full" />
-            <span className="font-medium">{card.lore.name}</span>
-          </div>
-        </div>
-      ))
-    ) : (
-      <div className="p-4 text-center text-gray-300">
-        Click on cards to add them to the deck.
+      <div className="flex flex-wrap gap-2 justify-center w-full">
+        <input
+          type="text"
+          value={deckName}
+          onChange={handleDeckNameChange}
+          style={{ outline: deckNameValid ? "none" : "2px solid red" }}
+          className="flex-shrink min-w-0 px-2 py-2 border rounded-md text-black bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent m-1.5 placeholder-gray-700 flex-basis[auto] max-w-full"
+          placeholder="Deck name"
+        />
       </div>
-    )}
+
+      {/* Save and Cancel Buttons */}
+      <div className="flex flex-wrap gap-2 justify-center w-full">
+        <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400" onClick={handleSave}>
+          ✓Save
+        </Button>
+        <Button variant="secondary" className="border-2 border-yellow-500 normal-case hover:scale-105 font-fable text-xl hover:border-yellow-400" onClick={onCancel}>
+          ✕Cancel
+        </Button>
+      </div>
+
+      {/* List of Cards in the Deck */}
+      <div className="mt-4 w-full">
+      {selectedCards.length > 0 ? (
+        selectedCards.map((card, index) => (
+          <div 
+            key={index} 
+            className="p-2 cursor-pointer hover:bg-gray-100"
+            onClick={() => onCardSelect(card)}
+          >
+            <div className="flex items-center space-x-3">
+              <Image src={testCards.find(tc => tc.id === card.id)?.image || '/card_art/1.jpg'} alt="Card art" width={40} height={40} className="object-cover rounded-full" />
+              <span className="font-medium">{card.lore.name}</span>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="p-4 text-center text-gray-300">
+          Click on cards to add them to the deck.
+        </div>
+      )}
+      </div>
     </div>
-  </div>
   )
 }
 
