a3cbca6591e0c7d15886b481b1cf23b24dba39f7
Aritra Chatterjee <aritrac1998@hotmail.com>
2024-02-27 17:24:02 +0530

    fix: remove unused import from eslintrc

--- packages/webapp/.eslintrc.cjs
+++ packages/webapp/.eslintrc.cjs
@@ -1,33 +1,29 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-	extends: [
-		'eslint:recommended, next/core-web-vitals',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-	],
-	parser: '@typescript-eslint/parser',
+	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier"],
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		project: "./tsconfig.json",
 	},
-	plugins: ['@typescript-eslint'],
+	plugins: ["@typescript-eslint"],
 	root: true,
-	ignorePatterns: ['node_modules', 'src/hooks/useScrollBox.ts'],
+	ignorePatterns: ["node_modules", "src/hooks/useScrollBox.ts"],
 	rules: {
-		'@typescript-eslint/no-unsafe-argument': 'off',
-		'@typescript-eslint/restrict-template-expressions': 'off',
-		'react/no-unescaped-entities': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
+		"@typescript-eslint/no-unsafe-argument": "off",
+		"@typescript-eslint/restrict-template-expressions": "off",
+		"react/no-unescaped-entities": "off",
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
 
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': [
-			'warn',
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
 			{
 				// ignore unused args that start with underscore
-				argsIgnorePattern: '^_',
-				varsIgnorePattern: '^_',
-				caughtErrorsIgnorePattern: '^_',
+				argsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+				caughtErrorsIgnorePattern: "^_",
 			},
 		],
 	},
